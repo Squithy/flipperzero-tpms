25bc79d7c5d9fa3af174c982186c905789bdba2e
Squithy <60709023+Squithy@users.noreply.github.com>
2025-01-25 11:17:10 +0000

    Added New protocols

--- protocols/protocol_items.h
+++ protocols/protocol_items.h
@@ -2,5 +2,8 @@
 #include "../tpms_app_i.h"
 
 #include "schrader_gg4.h"
+#include "SMD3MA4.h"
+#include "ford.h"
+#include "schrader_bc5a4.h"
 
 extern const SubGhzProtocolRegistry tpms_protocol_registry;
