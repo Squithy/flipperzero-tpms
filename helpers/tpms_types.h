aa51b3406ecc6afc5bcc8c2eb2685ef3b3dbe343
Squithy <60709023+Squithy@users.noreply.github.com>
2025-01-25 17:11:55 +0000

    Update

--- helpers/tpms_types.h
+++ helpers/tpms_types.h
@@ -3,9 +3,9 @@
 #include <furi.h>
 #include <furi_hal.h>
 
-#define TPMS_VERSION_APP "0.1"
-#define TPMS_DEVELOPED "wosk"
-#define TPMS_GITHUB "https://github.com/beewosk/flipperzero-tpms"
+#define TPMS_VERSION_APP "0.2.5"
+#define TPMS_DEVELOPED "iSquithy - Forked and enhanced from the work done by wosk"
+#define TPMS_GITHUB "https://github.com/squithy/flipperzero-tpms"
 
 #define TPMS_KEY_FILE_VERSION 1
 #define TPMS_KEY_FILE_TYPE "Flipper Tire Pressure Monitoring System Key File"
